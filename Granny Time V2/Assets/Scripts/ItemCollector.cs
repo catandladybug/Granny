ac48b81572e5dd46e63f0894ac1c46579ca2f5d7
catandladybug <98188215+catandladybug@users.noreply.github.com>
2023-09-25 20:34:45 -0700

    V2

---
+++ Granny Time V2/Assets/Scripts/ItemCollector.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemCollector : MonoBehaviour
+{
+    private int clues = 0;
+
+    [SerializeField] private Text clueText;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Clue"))
+        {
+            Destroy(collision.gameObject);
+            clues++;
+            clueText.text = "Clues: " + clues;
+        }
+    }
+}
