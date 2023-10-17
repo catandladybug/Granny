043594f51eb7be3f0be8014908fd587c542b4178
catandladybug <98188215+catandladybug@users.noreply.github.com>
2023-10-16 17:42:45 -0700

    upload

---
+++ Granny Time/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialogue : MonoBehaviour
+{
+    public TextMeshProUGUI textComponent;
+    public GameObject granny1; 
+    public GameObject granny2;
+    public GameObject bg;
+    public string[] lines;
+    public float textSpeed;
+
+    private int index;
+
+    void Start()
+    {
+        textComponent.text = string.Empty;
+        StartDialogue();
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(textComponent.text == lines[index])
+            {
+                NextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                textComponent.text = lines[index];
+            }
+        }
+    }
+
+    void StartDialogue()
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach(char c in lines[index].ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    void NextLine()
+    {
+        if(index < lines.Length - 1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+            granny1.SetActive(false);
+            granny2.SetActive(true);
+        }
+        else
+        {
+            gameObject.SetActive(false);
+            bg.SetActive(false);
+            //granny2.SetActive(false);
+        }
+    }
+}
